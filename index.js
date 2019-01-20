2a92ef3db1d4c23903b37ddad947da6de64c288c
Tony Bradley <tonybradley@Tonys-Mac-mini.fios-router.home>
2019-01-20 17:52:15 -0500

    make example even more simple

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const xs = require('xstream').default
 const run = require('@cycle/run').default
-const { makeHTTPDriver } = require('@cycle/http')
 
 // When merging multiple sources into a single stream
 // and using the results for different sinks, only the FIRST
@@ -9,37 +8,26 @@ const { makeHTTPDriver } = require('@cycle/http')
 // change whether or not we console.log "GOT FOO"
 
 const drivers = {
-  HTTP: makeHTTPDriver(),
-  FOO: (in$) => {
-    in$.addListener({
-      next: function (v) {
-        console.log('GOT FOO', v)
-      }
-    })
-  }
+  BAR: () => xs.periodic(1000),
+  FOO: (in$) => in$.addListener({
+    next: function (v) {
+      console.log('GOT FOO', v)
+    }
+  })
 }
 
 function app (sources) {
   const allSinks$ = xs.merge(
-    xs.of({ url: 'http://localhost:8082', category: 'test' }),
     xs.of({ foo: 1 }),
-    sources.HTTP.select('test')
-      .flatten()
-      .map((res) => ({ response: res.text }))
+    sources.BAR.mapTo({ bar: 1})
   )
 
   return {
-    HTTP: allSinks$.filter((v) => !!v.url),
-    FOO: allSinks$.filter((v) => !!v.foo)
+    BAR: allSinks$,
+    FOO: allSinks$.filter((v) => !!v.foo),
   }
 }
 
 run(app, drivers)
 
-require('http').createServer((req, res) => {
-  const buff = Buffer.from('hello')
-  res.writeHead(200, {
-    'Content-Length': buff.byteLength,
-    'Content-Type': 'text/plain' })
-  res.write(buff, () => res.end())
-}).listen(8082)
+require('http').createServer(() => {}).listen(8082)
